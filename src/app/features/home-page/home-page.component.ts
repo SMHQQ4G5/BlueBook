81305de03e2ecc515f782b4aaf8fb844d3869c06
Argel Joseph Pamintuan <argeljoseph.pamintuan@infor.com>
2025-07-26 20:47:12 +0800

    use dynamic component to home page tab content

--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -7,12 +7,8 @@ import { Tab1Component } from './components/tab1/tab1.component';
 import { Tab2Component } from './components/tab2/tab2.component';
 import { Tab3Component } from './components/tab3/tab3.component';
 
-const MODULE_DEPENDENCIES: any = [MatTabsModule, RouterModule, MatSidenavModule];
-const COMPONENT_DEPENDENCIES: any = [
-  Tab1Component,
-  Tab2Component,
-  Tab3Component,
-];
+const MODULE_DEPENDENCIES: any = [MatTabsModule, RouterModule, MatSidenavModule, CommonModule];
+const COMPONENT_DEPENDENCIES: any = [];
 
 @Component({
   selector: 'app-home-page',
@@ -26,11 +22,11 @@ const COMPONENT_DEPENDENCIES: any = [
 export class HomePageComponent {
 
   selectedTabIndex = 0;
-
-  tabs = [
-    {label: 'Recent Discussions', path: ''},
-    {label: 'Trending Discussions', path: 'tab2'},
-    {label: 'Announced Discussions', path: 'tab3'},
+  selectedTab: any = null;
+  tabs =  [
+    { label: 'Recent Discussions', component: Tab1Component },
+    { label: 'Trending Discussions', component: Tab2Component },
+    { label: 'Announced Discussions', component: Tab3Component },
   ];
 
   constructor(private route: Router, private activatedRoute: ActivatedRoute) {}
@@ -38,14 +34,17 @@ export class HomePageComponent {
   ngOnInit() {
     // Set the initial selected index based on the current route
     this.selectedTabIndex = parseInt(this.activatedRoute.snapshot.queryParamMap.get('tab') || '0');
+    this.selectedTab = (this.tabs[this.selectedTabIndex] || this.tabs[0]).component;
   }
 
   onTabChanged(idx: number) {
     this.selectedTabIndex = idx;
     if (this.selectedTabIndex < 1) {
+      this.selectedTab = this.tabs[0].component;
       this.route.navigate(['home']);
     }
     else {
+      this.selectedTab = (this.tabs[this.selectedTabIndex] || this.tabs[0]).component;
       this.route.navigate(['home'], {queryParams: {tab: this.selectedTabIndex}});
     }
   }
